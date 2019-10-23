b45484624a8477c21ee7bbdb5694ce83869e5e7f
Isac Cavalcante <isac.cavalcante@hp.com>
2019-10-23 17:04:47 -0300

    Add cleanup function

--- test/my_awesome_module_test.go
+++ test/my_awesome_module_test.go
@@ -8,13 +8,12 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-
+var t = &testing.T{}
+var m string
 
 func theTerraformModuleIsDeployed(module string) error {
-	var t = &testing.T{}
-
+	m = module
 	modulePath := "../modules/" + module
-
 	options := terraform.Options{
 		TerraformDir: modulePath,
 	}
@@ -32,7 +31,26 @@ func iCanAccessTheInstanceViaSSH() error {
 	return godog.ErrPending
 }
 
+func cleanup() error {
+	modulePath := "../modules/" + m
+	options := terraform.Options{
+		TerraformDir: modulePath,
+	}
+	out, err := terraform.DestroyE(t, &options)
+	if err != nil {
+		fmt.Println(out, err)
+		err := fmt.Errorf("[-] Error applying module %s", m)
+		return err
+	}
+	fmt.Println("[+] Module", m, "destroyed sucessfully")
+	return nil
+}
+
 func FeatureContext(s *godog.Suite) {
 	s.Step(`^the terraform module "([^"]*)" is deployed$`, theTerraformModuleIsDeployed)
 	s.Step(`^I can access the instance via SSH$`, iCanAccessTheInstanceViaSSH)
+
+	s.AfterScenario(func(interface{}, error) {
+		cleanup()
+	})
 }
